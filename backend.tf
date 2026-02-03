c75453d2da3521eaccccd0f52efbe015a4021345
Varaprasad Kasturi <vk67220@corp.cyient.com>
2026-02-03 12:04:45 +0530

    added

--- backend.tf
+++ backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "my-eks-terraform-state"
-    key            = "eks/${var.cluster_name}/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
